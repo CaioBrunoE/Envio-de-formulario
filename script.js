29a1ef9fb0f8784d5c970c8b84b492dfbf322b80
Caio Bruno <caiobruno90@yahoo.com>
2023-03-15 21:31:24 -0300

    Atualizacao01t

--- script.js
+++ script.js
@@ -21,38 +21,52 @@ submitButton.addEventListener('click', (event) => {
   // Validar o campo de nome
   if (nameInput.value.trim() === '') {
     nameInput.style.borderColor = 'red';
+    nameInput.style.color='red'
     isValid = false;
-  } else {
-    nameInput.style.borderColor = '';
+  
+  }else {
+    nameInput.style.borderColor = '#afeeee';
+    nameInput.style.color='#afeeee'
+
   }
+  
 
   // Validar o campo de sobrenome
   if (lastNameInput.value.trim() === '') {
     lastNameInput.style.borderColor = 'red';
+    lastNameInput.style.color='red'
     isValid = false;
   } else {
-    lastNameInput.style.borderColor = '';
+    lastNameInput.style.borderColor = '#afeeee';
+    lastNameInput.style.color='#afeeee'
   }
 
   // Validar o campo de endereço de e-mail
   if (addressInput.value.trim() === '' || !addressInput.value.includes('@')) {
     addressInput.style.borderColor = 'red';
+    addressInput.style.color='red'
     isValid = false;
   } else {
-    addressInput.style.borderColor = '';
+    addressInput.style.borderColor = '#afeeee';
+    addressInput.style.color='#afeeee'
   }
 
   // Validar o campo de senha
   if (passwordInput.value.trim() === '') {
     passwordInput.style.borderColor = 'red';
+    passwordInput.style.color='red'
     isValid = false;
   } else {
-    passwordInput.style.borderColor = '';
+    passwordInput.style.borderColor = '#afeeee';
+    passwordInput.style.color='#afeeee'
   }
 
   // Se o formulário for válido, envie-o e exiba o alerta
   if (isValid) {
     document.getElementById("login").submit();
     alert("Formulário enviado com sucesso!");
+    
+  }else{
+    alert("Formulario não foi prenchico corretamente!")
   }
 });
